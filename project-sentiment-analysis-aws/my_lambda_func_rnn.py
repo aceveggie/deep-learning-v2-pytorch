d2dc39fe4030f8f85c6549805506f165163caec0
rahul <kavirahul085@gmail.com>
2020-05-23 09:32:34 -0700

    added final project using AWS

---
+++ project-sentiment-analysis-aws/my_lambda_func_rnn.py
@@ -0,0 +1,21 @@
+# We need to use the low-level library to interact with SageMaker since the SageMaker API
+# is not available natively through Lambda.
+import boto3
+import json
+
+def lambda_handler(event, context):
+
+    # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
+    runtime = boto3.Session().client('sagemaker-runtime')
+
+    # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
+    response = runtime.invoke_endpoint(EndpointName = '', ContentType = 'text/plain', Body = event['body'])                       # The actual review
+
+    # The response is an HTTP response whose body contains the result of our inference
+    result = response['Body'].read().decode('utf-8')
+
+    return {
+        'statusCode' : 200,
+        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+        'body' : result
+    }
